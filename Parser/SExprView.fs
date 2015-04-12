60e59470388a8ae8cd94652893ae9aeb8d193460
Ming-Tang <project.shinkirou@gmail.com>
2015-04-11 17:57:34 -0700

    Implement formatter

--- Parser/SExprView.fs
+++ Parser/SExprView.fs
@@ -3,6 +3,7 @@ module Scheme.SExprView
 
 /// Parse tree of an S-expression
 [<AutoOpen>]
+[<StructuredFormatDisplay("{Format}")>]
 type SExprView =
 | NilV
 | FalseV
@@ -16,3 +17,30 @@ type SExprView =
 | UnquoteV of SExprView
 | ProperListV of SExprView list
 | DottedListV of SExprView list * SExprView
+
+let rec format v =
+  match v with
+  | NilV -> "()"
+  | FalseV -> "#f"
+  | TrueV -> "#t"
+  | IntV i -> string i
+  | RealV r -> string r
+  | StrV s -> sprintf "%A" s
+  | SymV s -> s
+  | QuoteV s -> sprintf "'%s" (format s)
+  | QuasiquoteV s -> sprintf "`%s" (format s)
+  | UnquoteV s -> sprintf ",%s" (format s)
+  | ProperListV xs ->
+    xs
+    |> List.map format
+    |> String.concat " "
+    |> sprintf "(%s)"
+  | DottedListV(xs, y) ->
+    let es =
+      xs
+      |> List.map format
+      |> String.concat " "
+    sprintf "(%s . %s)" es (format y)
+
+type SExprView with
+  member v.Format = format v
