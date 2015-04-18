6f7b7acaddc8ca4555b76f3c6cbe056ea4eda2fc
Ming-Tang <project.shinkirou@gmail.com>
2015-04-18 16:30:33 -0700

    Implement numeric functions

--- Scheme/Primitives.fs
+++ Scheme/Primitives.fs
@@ -37,24 +37,75 @@ let inline numeric init intOp realOp : Prim =
   let op = numericOp intOp realOp
   List.fold op init
 
+let inline numericUnOp' intOp realOp = fun (Args1(x)) ->
+  match x with
+  | Int i -> intOp i
+  | Real r -> realOp r
+  | x -> failwith "Non-numeric argument: %s" (Expr.format x)
+
+let inline numericUnOp intOp realOp =
+  numericUnOp' (intOp >> Int) (realOp >> Real)
+
 /// Require two or more arguments
 let req2OrMore (Args2OrMore xs) = xs
 
+/// Chain comparison
 let chainOp bin =
   fun (Args2OrMore xs) ->
     Seq.windowed 2 xs
     |> Seq.forall (fun ab -> bin ab.[0] ab.[1])
     |> createBool
 
+/// Binary operator of integers only
+let intBinOp f = fun (Args2(IntOnly a, IntOnly b)) -> f a b |> Int
+
+/// A rounding operation (round reals into ints)
+let roundOp f = numericUnOp' id (f >> int) >> Int
+
+/// An operation that act on reals and return reals
+let realOp f = numericUnOp' (float >> f) f >> Real
+
 let add : Prim = List.fold (numericOp (+) (+)) (Int 0)
 let sub : Prim = List.reduce (numericOp (-) (-))
 let mul : Prim = List.fold (numericOp ( * ) ( * )) (Int 1)
 let div : Prim = List.reduce (numericOp (/) (/))
+let quotient : Prim = intBinOp (/)
+let remainder : Prim = intBinOp (%)
+let modulo : Prim = intBinOp (fun n m -> ((n % m) + m) % m)
+
 let numEq : Prim = chainOp (numericOp' (=) (=))
 let numLess : Prim = chainOp (numericOp' (<) (<))
 let numGreater : Prim = chainOp (numericOp' (>) (>))
 let numLessEq : Prim = chainOp (numericOp' (<=) (<=))
 let numGreaterEq : Prim = chainOp (numericOp' (>=) (>=))
+
+let abs' : Prim = numericUnOp abs abs
+let floor' : Prim = roundOp floor
+let ceiling : Prim = roundOp ceil
+let truncate' : Prim = roundOp truncate
+let round' : Prim = roundOp round
+
+let exp' : Prim = realOp exp
+let sin' : Prim = realOp sin
+let cos' : Prim = realOp cos
+let tan' : Prim = realOp tan
+let asin' : Prim = realOp asin
+let acos' : Prim = realOp acos
+let atan' : Prim = fun args ->
+  match args with
+  | [_] -> numericUnOp' (float >> atan >> Real) (atan >> Real) args
+  | [a; b] -> numericOp' (fun a b -> atan2 (float a) (float b))
+                         (fun a b -> atan2 a b) a b |> Real
+  | _ -> failwith "atan2: Need one or two numeric arguments"
+let sqrt' : Prim = realOp sqrt
+let expt : Prim = fun (Args2(a, b)) ->
+  match a, b with
+  | Real r, Int i -> pown r i |> Real
+  | Int b, Int i -> pown b i |> Int
+  | Int i, Real r -> (float i) ** r |> Real
+  | Real r, Real p -> r ** p |> Real
+  | _, _ -> failwith "expt: Expecting two numeric arguments"
+
 
 let stringLength : Prim = fun (Args1(StrOnly s)) -> Int s.Length
 let stringRef : Prim = fun (Args2(StrOnly s, IntOnly i)) -> Str (string s.[i])
@@ -159,6 +210,26 @@ let standardPrimitives : Primitives =
     ">", numGreater
     "<=", numLessEq
     ">=", numGreaterEq
+
+    "quotient", quotient
+    "remainder", remainder
+    "modulo", modulo
+
+    "abs", abs'
+    "floor", floor'
+    "ceiling", ceiling
+    "truncate", truncate'
+    "round", round'
+
+    "exp", exp'
+    "sin", sin'
+    "cos", cos'
+    "tan", tan'
+    "asin", asin'
+    "acos", acos'
+    "atan", atan'
+    "sqrt", sqrt'
+    "expt", expt
 
     "string-length", stringLength
     "string-ref", stringRef
